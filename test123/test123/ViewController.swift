d576587fdb2788737e08075a7f2a48e3e43371c7
Eddie <Eddie.chen@tipsoftware.com>
2021-08-20 17:05:09 +0800

    first commit

---
+++ test123/test123/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test123
+//
+//  Created by Eddie on 2021/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("test123")
+    }
+
+
+}
+
